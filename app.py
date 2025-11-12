069c23a46e714faec6357183bb216d8fc419b2bc
Sergio LMC <you@example.com>
2025-11-12 08:41:23 -0600

    Initial FastAPI scaffold

---
+++ app.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+def home():
+    return "<h1>Loan Mountain Capital Estimator</h1><p>Beta version running!</p>"
